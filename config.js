d8d25f89cbc54dde0d5b7eb08d7f9b033e0b7ac9
Ariel Nuñez <ariel.nunez.sa@gmail.com>
2026-02-08 16:58:47 -0300

    Mejoras en index.html y project_1.html

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-export const MAIN_NAME = "Ariel Nuñez S.";
+export const main_name = "Ariel Nuñez S.";
+export const header_main_page = "Principal";
+export const header_contact_name = "Contacto";
+export const header_about_name = "Información";
+export const header_project_name = "Proyectos";
